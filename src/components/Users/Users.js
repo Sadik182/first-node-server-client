03c875c6cfb17876e8e1df2238e402808d5ce7a6
sadik182 <sadikurrahman728@gmail.com>
2022-10-20 20:32:36 +0600

    fatching data from server and view in table also added fontawesome

--- src/components/Users/Users.js
+++ src/components/Users/Users.js
@@ -1,11 +1,49 @@
-import React from 'react';
+import { faDeleteLeft, faRefresh, faTrash, faTrashAlt } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import React, { useEffect, useState } from "react";
+import { Button, Table } from "react-bootstrap";
 
 const Users = () => {
-    return (
-        <div>
-            <h1>This is Users Component</h1>
-        </div>
-    );
+  const [users, setUsers] = useState([]);
+
+  useEffect(() => {
+    fetch("http://localhost:5000/users")
+      .then((res) => res.json())
+      .then((data) => setUsers(data));
+  }, []);
+  return (
+    <div>
+      <Table striped bordered hover>
+        <thead>
+          <tr>
+            <th>ID</th>
+            <th>Full Name</th>
+            <th>Email</th>
+            <th>Phone</th>
+            <th>Action</th>
+          </tr>
+        </thead>
+        <tbody>
+            {
+                users.map(user => {
+                    return (
+                        <tr>
+                        <td>{user.id}</td>
+                        <td>{user.name}</td>
+                        <td>{user.email}</td>
+                        <td>{user.phone}</td>
+                        <td>
+                           <Button variant='outline-primary'>Delete  <FontAwesomeIcon icon={faTrashAlt} /></Button> {' /'}
+                            <Button variant='outline-primary ms-2'>Update <FontAwesomeIcon icon={faRefresh} /></Button>
+                        </td>
+                    </tr>
+                    )
+                })
+            }
+        </tbody>
+      </Table>
+    </div>
+  );
 };
 
 export default Users;
